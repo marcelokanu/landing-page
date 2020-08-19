a1ca3b374e9bd805e74d01c8a1f3a8718765e3e5
Marcelo Bonilla <marcelokanu@gmail.com>
2020-08-18 21:18:08 -0400

    aulas 65 - 70

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -6,14 +6,19 @@ import * as S from './styles'
 
 import { gaEvent } from 'utils/ga'
 import Container from 'components/Container'
+import { LogoProps } from 'types/api'
 
 const onClick = () =>
   gaEvent({ action: 'click', category: 'cta', label: 'hero button' })
 
-const SectionHero = () => (
+type Props = {
+  logo: LogoProps
+}
+
+const SectionHero = ({ logo }: Props) => (
   <S.Wrapper>
     <Container>
-      <Logo />
+      <Logo {...logo} />
 
       <S.Content>
         <S.TextBlock>
@@ -23,7 +28,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=BESTSELLER"
+              href="https://www.udemy.com/course/react-avancado/?couponCode=MODULO1"
               onClick={onClick}
               wide
             >

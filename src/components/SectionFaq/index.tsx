8cf588604c28a3eb45b80dfe4695a48a7a32b376
Marcelo Bonilla <marcelokanu@gmail.com>
2020-08-21 11:58:29 -0400

    aulas 71 - 80

--- src/components/SectionFaq/index.tsx
+++ src/components/SectionFaq/index.tsx
@@ -3,44 +3,35 @@ import React from 'react'
 import Container from 'components/Container'
 import Heading from 'components/Heading'
 
-import faq from './content'
 import * as S from './styles'
+import { SectionFaqProps } from 'types/api'
 
-const SectionFaq = () => (
+const SectionFaq = ({ title, questions }: SectionFaqProps) => (
   <S.Wrapper>
     <S.Content>
       <Container>
-        <Heading>FAQ</Heading>
+        <Heading>{title}</Heading>
 
         <S.Questions>
-          {faq.map(({ question, answer }, index) => (
-            <S.Question key={index}>
-              <Heading lineBottom>{question}</Heading>
-              <div dangerouslySetInnerHTML={{ __html: answer }} />
-            </S.Question>
-          ))}
+          {questions
+            .filter((question) => question.isExtra === false)
+            .map(({ question, answer }, index) => (
+              <S.Question key={index}>
+                <Heading lineBottom>{question}</Heading>
+                <div dangerouslySetInnerHTML={{ __html: answer }} />
+              </S.Question>
+            ))}
         </S.Questions>
 
         <S.ExtraQuestion>
-          <Heading lineBottom>Eu tenho outra dúvida!</Heading>
-          <p>
-            Sem problemas! Você pode acessar qualquer uma das{' '}
-            <a
-              href="https://willianjusten.com.br/about"
-              target="_blank"
-              rel="noreferrer"
-            >
-              minhas redes sociais
-            </a>{' '}
-            ou entrar no{' '}
-            <a
-              href="https://slack-willianjusten.herokuapp.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              slack do nosso curso.
-            </a>
-          </p>
+          {questions
+            .filter((question) => question.isExtra === true)
+            .map(({ question, answer }, index) => (
+              <S.Question key={index}>
+                <Heading lineBottom>{question}</Heading>
+                <div dangerouslySetInnerHTML={{ __html: answer }} />
+              </S.Question>
+            ))}
         </S.ExtraQuestion>
       </Container>
     </S.Content>
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,121 @@
-export type LogoProps = {
+export type Image = {
   alternativeText: string
   url: string
 }
 
+export type Button = {
+  url: string
+  label: string
+}
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button?: Button
+  image: Image
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: Image
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: Array<{
+    title: string
+  }>
+}
+
+export type Modules = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Modules[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: Button
+}
+
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  description: string
+  socialLinks: SocialLink[]
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
+export type Review = {
+  id: number
+  name: string
+  text: string
+  photo: {
+    url
+  }
+}
+export type SectionReviewsProps = {
+  title: string
+  reviews: Review[]
+}
+
+export type Question = {
+  question: string
+  answer: string
+  isExtra: boolean
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: Question[]
+}
+
 export type LandingPageProps = {
-  logo: LogoProps
+  logo: Image
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
